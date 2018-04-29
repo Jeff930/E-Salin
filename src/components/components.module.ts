6370407d0fea1aa4bdff58d0d512b46a53a6a481
Jeff930 <jefferson30.jdg@gmail.com>
2018-04-30 01:31:14 +0800

    Added a TranslatePage and Dictionary Page

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { TabsPageComponent } from './tabs-page/tabs-page';
-@NgModule({
-	declarations: [TabsPageComponent],
-	imports: [],
-	exports: [TabsPageComponent]
-})
-export class ComponentsModule {}
