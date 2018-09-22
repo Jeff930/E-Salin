d594d02f3cbc207066b2fd760981b724c0fcf3de
Unknown <jefferson30.jdg@gmail.com>
2018-09-22 22:33:04 +0800

    Added interface for Help page

---
+++ src/pages/help-tab/help-tab.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the HelpPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-helptab',
+  templateUrl: 'helptab.html',
+})
+export class HelpTabPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HelpPage');
+  }
+
+}
