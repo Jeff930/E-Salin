3c8cf9bd1d243c9793405e6d07feccffa3a7bf50
Unknown <jefferson30.jdg@gmail.com>
2018-09-27 23:40:33 +0800

    Added methods to import database

--- src/providers/database-handler/database-handler.ts
+++ src/providers/database-handler/database-handler.ts
@@ -1,6 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
+import { SQLitePorter } from '@ionic-native/sqlite-porter';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { BehaviorSubject } from 'rxjs/Rx';
+import { Storage } from '@ionic/storage';
 /*
   Generated class for the DatabaseHandlerProvider provider.
 
@@ -10,8 +15,40 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class DatabaseHandlerProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello DatabaseHandlerProvider Provider');
+  database: SQLiteObject;
+  private databaseReady: BehaviorSubject<boolean>;
+ 
+  constructor(public sqlitePorter: SQLitePorter, private storage: Storage, private sqlite: SQLite, private platform: Platform, private http: HttpClient) {
+    this.databaseReady = new BehaviorSubject(false);
+    this.platform.ready().then(() => {
+      this.sqlite.create({
+        name: 'developers.db',
+        location: 'default'
+      })
+        .then((db: SQLiteObject) => {
+          this.database = db;
+          this.storage.get('database_filled').then(val => {
+            if (val) {
+              this.databaseReady.next(true);
+            } else {
+              this.fillDatabase();
+            }
+          });
+        });
+    });
+  }
+ 
+  fillDatabase() {
+    this.http.get('assets/dummyDump.sql')
+      .map(res => res.text())
+      .subscribe(sql => {
+        this.sqlitePorter.importSqlToDb(this.database, sql)
+          .then(data => {
+            this.databaseReady.next(true);
+            this.storage.set('database_filled', true);
+          })
+          .catch(e => console.error(e));
+      });
   }
 
 }
