ead43bf0b662d12c2c25ee9f2a99de7fa5a41310
Unknown <jefferson30.jdg@gmail.com>
2018-09-29 23:46:08 +0800

    Created a modal for displaying dictionary results

---
+++ src/components/dictionary-modal/dictionary-modal.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the DictionaryModalComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'dictionary-modal',
+  templateUrl: 'dictionary-modal.html'
+})
+export class DictionaryModalComponent {
+
+  text: string;
+
+  constructor() {
+    console.log('Hello DictionaryModalComponent Component');
+    this.text = 'Hello World';
+  }
+
+}
